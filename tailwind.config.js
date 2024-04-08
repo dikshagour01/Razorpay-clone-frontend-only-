56418b3d44f2c53ed3c1da227bf82d0ebf751d6e
dikshagour01 <131194061+dikshagour01@users.noreply.github.com>
2024-04-08 15:28:09 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: { colors: {
+      deepblue: '#0c0a2b', // Replace this with your chosen color code
+    },
+  },
+  },
+  plugins: [],
+}
+
